9e8b5bf649a35ddca4b8d3239500253558c80395
ZZMHunter <zhao.zhang50@myhunter.cuny.edu>
2025-11-30 13:33:23 -0500

    getFullName return function typo

--- social4.cpp
+++ social4.cpp
@@ -36,7 +36,7 @@ class Profile{
         }
         string getFullName(){
             string result = displayName;
-            result += " (@)";
+            result += " (@";
             result += username;
             result += ")";
             return result;
